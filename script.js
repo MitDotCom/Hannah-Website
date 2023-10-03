67482139161071fe68544a1df7931e1ae558a84b
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-02 17:51:10 -0700

    Add images and begin adding CSS structure to images

---
+++ script.js
@@ -0,0 +1,16 @@
+let currentImage = document.getElementById("current-image");
+
+const imageThumbs = document.getElementById("image-thumbs");
+
+// for (let i = 1; 1 <= 10; i++) {
+//   let thumb = document.createElement("img");
+//   thumb.src = "images/image" + i + ".jpg";
+//   thumb.alt = "Image " + i;
+//   thumb.classList.add("thumb");
+//   imageThumbs.appendChild(thumb);
+
+//   thumb.addEventListener("click", function () {
+//     currentImage.src = this.src;
+//     currentImage.alt = this.alt;
+//   });
+// }
