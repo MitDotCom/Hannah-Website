56db05c9b30cea7a080fdcc023c03e13b8186edd
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-02 18:20:57 -0700

    DONE! I think... <3 love you Hannah

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let currentImage = document.getElementById("current-image");
 
 for (let i = 1; i <= 21; i++) {
   let thumb = document.createElement("img");
-  thumb.src = "images/image" + i + ".jpg";
+  thumb.src = "./images/image" + i + ".JPG";
   thumb.alt = "Image " + i;
   thumb.classList.add("thumb");
   imageThumbs.appendChild(thumb);
